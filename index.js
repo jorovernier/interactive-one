a3e7408eaf3aa377a7d016d88c8c35c5042ec207
jorovernier <55214971+jorovernier@users.noreply.github.com>
2022-06-08 12:19:34 -0700

    finished demo

---
+++ index.js
@@ -0,0 +1,40 @@
+// console.log('Hello, world!')
+
+let count = 0;
+
+const minusBtn = document.querySelector('#minus-btn');
+const resetBtn = document.querySelector('#reset-btn');
+const plusBtn = document.querySelector('#plus-btn');
+
+const counter = document.getElementById('counter');
+
+const buttons = document.querySelectorAll('.theme-buttons');
+
+function increase(){
+    count += 1
+    counter.textContent = count
+}
+
+const decrease = function(){
+    count -= 1
+    counter.textContent = count
+}
+
+const reset = () => {
+    count = 0
+    counter.textContent = count
+}
+
+function selectTheme(event){
+    let theme = event.target.textContent
+    document.querySelector('body').className = theme
+    document.querySelector('main').className = theme
+}
+
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', selectTheme)
+}
+
+minusBtn.addEventListener('click', decrease)
+resetBtn.addEventListener('click', reset)
+plusBtn.addEventListener('click', increase)
